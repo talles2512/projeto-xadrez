5a822fe9519354ca6124bc48d042369900b756f5
Hebert Talles <talles_be@hotmail.com>
2019-07-22 20:06:58 -0300

    Testes iniciais com movimentos possiveis das pecas

--- JogoDeXadrez/xadrez/Rei.cs
+++ JogoDeXadrez/xadrez/Rei.cs
@@ -13,5 +13,75 @@ namespace xadrez
         {
             return "R";
         }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca peca = Tabuleiro.Peca(pos);
+            return peca == null || peca.Cor != Cor;
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            //acima
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //diagonal superior direita
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //direita
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //diagonal inferior direita
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //abaixo
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //diagonal inferior esquerda
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //esquerda
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            //diagonal superior esquerda 
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+            }
+
+            return mat;
+        }
     }
 }
