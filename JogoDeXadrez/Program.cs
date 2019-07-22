533fbfdab245631514b0067c0f3e6f02817f4bec
Hebert Talles <talles_be@hotmail.com>
2019-07-22 02:38:15 -0300

    Comecanco a colocar pecas no tabuleiro

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace JogoDeXadrez
 {
@@ -9,7 +10,14 @@ namespace JogoDeXadrez
         {
             Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
+            tabuleiro.ColocarPeca(new Torre(Cor.Preta,tabuleiro), new Posicao(0, 0));
+            tabuleiro.ColocarPeca(new Torre(Cor.Preta,tabuleiro), new Posicao(1, 3));
+            tabuleiro.ColocarPeca(new Rei(Cor.Preta,tabuleiro), new Posicao(2, 4));
+
+
             Tela.ImprimirTabuleiro(tabuleiro);
+
+
             Console.ReadLine();
         }
     }
--- JogoDeXadrez/Tabuleiro/Peca.cs
+++ JogoDeXadrez/Tabuleiro/Peca.cs
@@ -7,9 +7,9 @@
         public int QteMovimentos { get; set; }
         public Tabuleiro Tabuleiro { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        public Peca(Cor cor, Tabuleiro tabuleiro)
         {
-            Posicao = posicao;
+            Posicao = null;
             Cor = cor;
             QteMovimentos = 0;
             Tabuleiro = tabuleiro;
